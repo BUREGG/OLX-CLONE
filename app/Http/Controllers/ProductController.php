7df9c8ac72582a1dfc9a1e38c03c9b277360a043
Maciej Burek <mburek@primeo.pl>
2024-05-16 12:09:35 +0200

    filtrowanie po cenie

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -23,6 +23,7 @@ class ProductController extends Controller
         $products = Product::where('is_active', true)->with('images', 'product_users')->get();
         return view('product', ['products' => $products]);
     }
+
     public function category($id)
     {
         $category = Category::where('id', $id)->with('children')->firstOrFail();
