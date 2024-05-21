ad7e14197c525b215f0e513a36f40c0b43a803cc
Maciej Burek <mburek@primeo.pl>
2024-05-21 16:02:56 +0200

    wyswietlanie top 3 ogłoszen

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,13 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
    public function getCategory()
    {
+      $products = Product::orderBy('views', 'desc')->take(3)->get();
       $categories = Category::root()->get();
-      return view('welcome', ['categories' => $categories]);
+      return view('welcome', ['categories' => $categories],['products' => $products]);
    }
 }
