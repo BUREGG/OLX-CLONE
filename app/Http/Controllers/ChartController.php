d2fb4aed5b9a4a59718ec5cdca3508efba0daa15
Maciej Burek <mburek@primeo.pl>
2024-05-23 14:09:41 +0200

    wykresy

--- app/Http/Controllers/ChartController.php
+++ app/Http/Controllers/ChartController.php
@@ -4,24 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
 use App\Models\Product;
-use LaravelDaily\LaravelCharts\Classes\LaravelChart;
-
 use Illuminate\Http\Request;
 
 class ChartController extends Controller
 {
-    public function chart(){
+    public function chart()
+    {
+        $user = Auth::user();
 
-    $chart_options = [
-        'chart_title' => 'Users by months',
-        'report_type' => 'group_by_string',
-        'model' => 'App\Models\Product',
-        'group_by_field' => 'views',
-        'group_by_period' => 'month',
-        'chart_type' => 'bar',
-    ];
-    $chart1 = new LaravelChart($chart_options);
-    
-    return view('chart', compact('chart1'));
-}
+        $products = Product::where('user_id', $user->id)->get(['name', 'views']);
+
+
+        return view('chart', compact('products'));
+    }
 }
